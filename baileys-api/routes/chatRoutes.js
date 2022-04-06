0d43036c099eb5f6ed023bc177971a18e4f2151b
LUIZ HENRIQUE <34195452+Luizhcrs@users.noreply.github.com>
2022-04-06 10:47:50 -0300

    api

---
+++ baileys-api/routes/chatRoutes.js
@@ -0,0 +1,48 @@
+import { Router } from 'express'
+import { body, query } from 'express-validator'
+import requestValidator from './../middlewares/requestValidator.js'
+import sessionValidator from './../middlewares/sessionValidator.js'
+import * as controller from './../controllers/chatController.js'
+import getMessages from './../controllers/getMessages.js'
+
+const router = Router()
+
+router.get('/get', query('id').notEmpty(), requestValidator, sessionValidator, controller.getList)
+
+router.get('/get/:jid', query('id').notEmpty(), requestValidator, sessionValidator, getMessages)
+
+router.post(
+    '/send',
+    query('id').notEmpty(),
+    body('receiver').notEmpty(),
+    body('message').notEmpty(),
+    requestValidator,
+    sessionValidator,
+    controller.send
+)
+router.post(
+    '/send-file',
+    query('id').notEmpty(),
+    body('receiver').notEmpty(),
+    body('link').notEmpty(),
+    body('message').notEmpty(),
+    requestValidator,
+    sessionValidator,
+    controller.sendFile
+)
+router.post(
+    '/send-pdf',
+    query('id').notEmpty(),
+    body('receiver').notEmpty(),
+    body('link').notEmpty(),
+    body('message').notEmpty(),
+    body('nome').notEmpty(),
+    requestValidator,
+    sessionValidator,
+    controller.sendPdf
+)
+
+
+router.post('/send-bulk', query('id').notEmpty(), requestValidator, sessionValidator, controller.sendBulk)
+
+export default router
